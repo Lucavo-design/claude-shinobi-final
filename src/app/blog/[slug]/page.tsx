e9c8225cf57b63b5a21677d5c45428956996d48c
vincent imbriani <vincent@lucavo.com>
2026-01-04 23:53:03 -0500

    test commit

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -6,11 +6,6 @@ import Link from 'next/link'
 import Avatar from '@/components/ui/Avatar/Avatar'
 import styles from './BlogPost.module.css'
 
-type Props = {
-  params: Promise<{ slug: string }>
-  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>
-}
-
 async function getSinglePost(slug: string): Promise<BlogPost | null> {
   const response = await fetch(process.env.HYGRAPH_ENDPOINT!, {
     method: "POST",
@@ -37,7 +32,8 @@ async function getSinglePost(slug: string): Promise<BlogPost | null> {
   return json.data.blogPost
 }
 
-export default async function BlogPostPage({ params }: Props) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default async function BlogPostPage({ params }: any) {
   const { slug } = await params
   const post = await getSinglePost(slug)
 
